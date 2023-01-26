c6d96b66d8ceb5a63689a4d94301cb8b19d7da1b
mafusailovich <vfesenko@yandex.ru>
2023-01-26 22:32:53 +0300

    Add tables in database
    Add tables: Users, PerevalAdded, Coords, Images,PerevalImages

---
+++ fstr/pereval/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Users(models.Model):
+    email = models.EmailField(max_length=255)
+    name = models.CharField(max_length=30)
+    fam = models.CharField(max_length=30)
+    otc = models.CharField(max_length=30)
+    phone = models.CharField(max_length=12)
+
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint (fields=['email'], name='unique_email')
+        ]
+
+
+class PerevalAdded(models.Model):
+    date_added = models.DateTimeField(auto_now=True)
+    beautytitle = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    other_titles = models.CharField(max_length=255)
+    connect = models.TextField(blank=True)
+    add_time = models.DateTimeField()
+    level_winter = models.CharField(max_length=255)
+    level_spring = models.CharField(max_length=255)
+    level_summer = models.CharField(max_length=255)
+    level_autumn = models.CharField(max_length=255)
+    coord = models.OneToOneField('Coords', on_delete=models.CASCADE)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+
+
+
+
+class Coords(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    height = models.IntegerField()
+
+
+class Images(models.Model):
+    title = models.CharField(max_length=255)
+    img = models.ImageField()
+
+
+class PerevalImages(models.Model):
+    pereval = models.ForeignKey(PerevalAdded, on_delete=models.CASCADE)
+    img = models.ForeignKey(Images, on_delete=models.CASCADE)
